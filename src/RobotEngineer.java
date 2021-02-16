402d58cd59a90971dda3ca5aa834e91cf4f1ecd1
kanishka424 <kanishkamalpana24@gmail.com>
2021-02-16 12:00:48 +0530

    addin Dreke banas builder pattern example see the test for decription

---
+++ src/RobotEngineer.java
@@ -0,0 +1,22 @@
+public class RobotEngineer {
+
+    private RobotBuilder robotBuilder;
+
+    public RobotEngineer(RobotBuilder robotBuilder) {
+        this.robotBuilder = robotBuilder;
+    }
+
+
+        public  Robot getRobot(){
+        return this.robotBuilder.getRobot();
+        }
+
+
+        public void makeRobot(){
+        this.robotBuilder.buildRobotArms();
+        this.robotBuilder.buildRobotTorso();
+        this.robotBuilder.buildRobotLegs();
+        this.robotBuilder.buildRobotHead();
+
+        }
+}
